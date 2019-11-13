79d371d434f0c88b7a84862e5cc2232b855fbeed
João Pedro <47194462+JhonIstone@users.noreply.github.com>
2019-11-13 19:59:50 -0300

    Add files via upload

--- Gene/Gene.java
+++ Gene/Gene.java
@@ -1,15 +1,23 @@
-public class Gene{
-     private int estadoGene;
-    
-    public void insereValorEstado(int x){
-        estadoGene = x;
+    public class Gene{
+    private int estadoGene;
+        
+    public Gene(int x){
+        this.setAltera(x);
+    };
+        
+    public void setAltera(int x){
+        this.estadoGene = x;
     };
     
-    public int retorna(){
-        return estadoGene;
+    public int getGene(){
+        return this.estadoGene;
     };
     
-    public boolean Equals(Gene gene2){
-        return estadoGene == gene2.estadoGene;
+    public boolean equals(Gene gene2){
+        return this.getGene() == gene2.getGene();
+    };
+    
+    public Gene clonar (){
+        return new Gene(this.getGene());
     };
 };
---
+++ Gene/TesteGene.java
@@ -0,0 +1,5 @@
+public class TesteGene{
+    Gene gene1 = new Gene(10);
+    Gene gene2 = new Gene(20);
+    Gene gene3 = new Gene(20);
+}
